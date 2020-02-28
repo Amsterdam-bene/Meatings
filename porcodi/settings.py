fd5a5edbd87c8ce519cc3d06b9b662befa384ea9
Antonio Malara <antonio.malara@gmail.com>
2020-02-28 22:53:45 +0100

    set the static and database file to be as siblings as the repo checkout dir

--- porcodi/settings.py
+++ porcodi/settings.py
@@ -79,7 +79,7 @@ WSGI_APPLICATION = 'porcodi.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'NAME': os.path.join(BASE_DIR, '..', 'db.sqlite3'),
     }
 }
 
@@ -121,3 +121,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, '..', 'static')
